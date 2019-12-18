2cb4b7a6d915fb374c1c31285240d2a55fa390f8
Marcin Fereniec <marcin.fereniec@gmail.com>
2019-12-18 22:10:37 +0100

    All DTOs

---
+++ src/main/java/pl/coderslab/charity/dtos/CategoryDTO.java
@@ -0,0 +1,10 @@
+package pl.coderslab.charity.dtos;
+
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/pl/coderslab/charity/dtos/DonationDTO.java
@@ -0,0 +1,34 @@
+package pl.coderslab.charity.dtos;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import pl.coderslab.charity.domain.entities.Category;
+import pl.coderslab.charity.domain.entities.Institution;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+public class DonationDTO {
+
+    private Long id;
+    private Integer quantity;
+    @NotBlank
+    private String street;
+    @NotBlank
+    private String city;
+    @NotBlank
+    private String zipCode;
+    @NotBlank @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate pickUpDate;
+    @NotBlank
+    private LocalTime pickUpTime;
+    @Size(max = 200)
+    private String pickUpComment;
+    private List<Category> categories;
+    private Institution institutions;
+
+}
