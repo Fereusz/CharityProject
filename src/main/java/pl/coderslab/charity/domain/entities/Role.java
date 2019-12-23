e50a70911c062d49a60c23a457a06a3e335a3d03
Marcin Fereniec <marcin.fereniec@gmail.com>
2019-12-23 20:34:46 +0100

    User & Role entities

---
+++ src/main/java/pl/coderslab/charity/domain/entities/Role.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter @Setter @ToString @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
---
+++ src/main/java/pl/coderslab/charity/domain/entities/User.java
@@ -0,0 +1,38 @@
+package pl.coderslab.charity.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = {"password", "donations"})
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private Boolean active = Boolean.FALSE;
+    @OneToMany(mappedBy = "user")
+    private List<Donation> donations = new ArrayList<>();
+    @ManyToMany
+    private Set<Role> roles = new HashSet<>();
+
+
+}
