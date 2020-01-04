f58ccab76fdf363e9d7378d56568c66bb887c63e
Marcin Fereniec <marcin.fereniec@gmail.com>
2020-01-04 14:57:24 +0100

    Admin main page

--- src/main/java/pl/coderslab/charity/controllers/AdminController.java
+++ src/main/java/pl/coderslab/charity/controllers/AdminController.java
@@ -7,21 +7,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.domain.entities.User;
+import pl.coderslab.charity.domain.repositories.UserRepository;
 import pl.coderslab.charity.dtos.RegistrationDTO;
+import pl.coderslab.charity.services.DonationService;
+import pl.coderslab.charity.services.InstitutionService;
 import pl.coderslab.charity.services.RegistrationService;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final RegistrationService registrationService;
+    private final UserRepository userRepository;
+    private final InstitutionService institutionService;
+    private final DonationService donationService;
 
-    public AdminController(RegistrationService registrationService) {
+
+
+    public AdminController(RegistrationService registrationService, UserRepository userRepository, InstitutionService institutionService, DonationService donationService) {
         this.registrationService = registrationService;
+        this.userRepository = userRepository;
+        this.institutionService = institutionService;
+        this.donationService = donationService;
     }
 
+    @GetMapping
+    public String getAdminPage (Principal principal, Model model) {
+        String name = principal.getName();
+        User user = userRepository.findByUsername(name);
+        model.addAttribute("admin", user);
+        model.addAttribute("institutions",institutionService.findAllInstitutions());
+        model.addAttribute("numberOfBags",donationService.sumOfBags());
+        model.addAttribute("numberOfInstitutions", donationService.sumOfDonatedInstitutions());
+        return "admin/admin-main";
+
+    }
 
     @GetMapping("/create")
     public String prepareAdminAccount(Model model) {
