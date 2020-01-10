89b216a9d7eeee6c59a1231b0e49a4051895d0bf
Marcin Fereniec <marcin.fereniec@gmail.com>
2020-01-10 14:42:41 +0100

    Donation controller for logged user - no user user_id for donation added - not able to pass user_id into JSP

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -12,8 +12,10 @@ import pl.coderslab.charity.dtos.DonationDTO;
 import pl.coderslab.charity.services.CategoryService;
 import pl.coderslab.charity.services.DonationService;
 import pl.coderslab.charity.services.InstitutionService;
+import pl.coderslab.charity.services.UserService;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/donation")
@@ -22,19 +24,22 @@ public class DonationController {
     private final CategoryService categoryService;
     private final InstitutionService institutionService;
     private final DonationService donationService;
+    private final UserService userService;
 
 
-    public DonationController(CategoryService categoryService, InstitutionService institutionService, DonationService donationService) {
+    public DonationController(CategoryService categoryService, InstitutionService institutionService, DonationService donationService, UserService userService) {
         this.categoryService= categoryService;
         this.institutionService = institutionService;
         this.donationService = donationService;
+        this.userService = userService;
     }
 
     @GetMapping("/form")
-    public String prepareDonationProcess (Model model) {
+    public String prepareDonationProcess (Model model, Principal principal) {
         model.addAttribute("donationDTO",new DonationDTO());
         model.addAttribute("categories",categoryService.findAllCategories());
         model.addAttribute("institutions", institutionService.findAllInstitutions());
+        model.addAttribute("user", userService.findUserByUsername(principal.getName()));
         return "form";
     }
     @PostMapping("/form")
